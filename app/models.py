5b82f6a027f2319df00af2dc6deda4f5574a4425
Shyanukant Rathi <72182251+shyanukant@users.noreply.github.com>
2024-02-15 15:53:48 +0000

    add model, schemas and main

---
+++ app/models.py
@@ -0,0 +1,13 @@
+from .database import Base
+from sqlalchemy import Column, String, Integer, Boolean
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+from sqlalchemy.sql.expression import text
+
+class Post(Base):
+    __tablename__ = 'posts'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    title = Column(String, nullable=False, )
+    content = Column(String, nullable=False)
+    published = Column(Boolean, server_default='True')
+    created_at = Column(TIMESTAMP(timezone=True), nullable=True, server_default=text('now()'))
