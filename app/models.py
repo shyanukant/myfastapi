d1b977f88ef2ac51e37c15d87df7757bb17b0f90
Shyanukant Rathi <72182251+shyanukant@users.noreply.github.com>
2024-02-23 17:33:31 +0000

    add vot model

--- app/models.py
+++ app/models.py
@@ -22,3 +22,9 @@ class User(Base):
     email = Column(String, unique=True, nullable=False)
     password = Column(String, unique=True, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=True, server_default=text('now()'))
+
+class Vote(Base):
+    __tablename__='votes'
+
+    post_id = Column(Integer, ForeignKey("posts.id", ondelete="CASCADE"), primary_key=True)
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), primary_key=True)
