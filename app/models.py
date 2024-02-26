4d7c47613cbd35fee6de242b69e10733869b9c3a
Shyanukant Rathi <72182251+shyanukant@users.noreply.github.com>
2024-02-26 12:04:42 +0000

    commit -m fix alembic

--- app/models.py
+++ app/models.py
@@ -21,7 +21,7 @@ class User(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     email = Column(String, unique=True, nullable=False)
     phone = Column(String, unique=True, nullable=False)
-    password = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=True, server_default=text('now()'))
 
 class Vote(Base):
